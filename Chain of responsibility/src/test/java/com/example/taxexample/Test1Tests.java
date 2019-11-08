c03692c4ecb8998bc4dadb1dd99568bda17c7031
gpr-indevelopment <49736266+gpr-indevelopment@users.noreply.github.com>
2019-11-07 21:07:50 -0300

    Added Chain of Responsibility folder

---
+++ Chain of responsibility/src/test/java/com/example/taxexample/Test1Tests.java
@@ -0,0 +1,13 @@
+package com.example.taxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
