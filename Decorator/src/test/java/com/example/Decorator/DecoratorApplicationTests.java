5be82d1047a2ddc98593ac2ff6861f982fce49b9
gpr-indevelopment <49736266+gpr-indevelopment@users.noreply.github.com>
2019-11-10 21:06:02 -0300

    Added Decorator module

---
+++ Decorator/src/test/java/com/example/Decorator/DecoratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Decorator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
